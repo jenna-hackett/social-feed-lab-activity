2b047f6282be0df39d742a83386ba14f214ccb89
Jenna Hackett <jenna.hackett96@gmail.com>
2026-03-11 14:20:01 -0600

    comments addded to code for readability.

--- src/services/classFeed.ts
+++ src/services/classFeed.ts
@@ -12,17 +12,43 @@ export async function login(username: string) {
 
 // Get Feed function that retrieves the list of posts from the server. For now, it returns a static list of posts as a placeholder.
 export async function getFeed(authors?: string) {
+  // COMMENTED OUT BECAUSE SERVER IS DOWN FOR API.
   //const response = await api.get("/feed", {
   // This passes the authors to the API as a query parameter: /feed?authors=...
   //params: authors ? { authors } : {},
   //});
   //return response.data.posts;
 
+  // MOCK DATA TO ALLOW APP TO FUNCTION WITHOUT API
   return [
     {
       id: "1",
       author: "Local_Test",
       text: "This is a sample post from the feed.",
+      createdAt: Date.now(),
+      commentCount: 0,
+      comments: [],
+    },
+    {
+      id: "2",
+      author: "Local_Test",
+      text: "This is a second sample post from the feed.",
+      createdAt: Date.now(),
+      commentCount: 0,
+      comments: [],
+    },
+    {
+      id: "3",
+      author: "Local_Test",
+      text: "This is a third sample post from the feed.",
+      createdAt: Date.now(),
+      commentCount: 0,
+      comments: [],
+    },
+    {
+      id: "4",
+      author: "Local_Test",
+      text: "This is a fourth sample post from the feed.",
       createdAt: Date.now(),
       commentCount: 0,
       comments: [],
